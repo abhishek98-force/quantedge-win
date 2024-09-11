33de939ff4cc6f4b280dcebd9addda5b3b3d37f1
bhavana-nair <bhavananair97@gmail.com>
2024-09-11 19:20:35 -0400

    mac changes

--- build-macos.py
+++ build-macos.py
@@ -1,11 +1,12 @@
 import os
 import shutil
-from distutils.core import setup
+from setuptools import setup
+
 
 
 def tree(src):
     return [
-        (root, map(lambda f: os.path.join(root, f), files))
+        (root, [os.path.join(root, f) for f in files])  # Convert map to list
         for (root, dirs, files) in os.walk(os.path.normpath(src))
     ]
 
@@ -19,11 +20,19 @@ if os.path.exists("dist/index.app"):
 ENTRY_POINT = ["backend/index.py"]
 
 DATA_FILES = tree("gui")
+
+DATA_FILES = tree("gui")
+
+# Print the tree structure
+print("Data Files Structure:", DATA_FILES)
+
+
+
 OPTIONS = {
     "argv_emulation": False,
     "strip": False,
     "iconfile": "backend/assets/logo.icns",
-    "packages": ["WebKit", "Foundation", "webview"],
+    "packages": ["WebKit", "Foundation", "webview","uvicorn","fastapi","anyio"],
     "plist": {"NSRequiresAquaSystemAppearance": False},
     "resources": DATA_FILES,
 }
